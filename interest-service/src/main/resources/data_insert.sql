cf91a06838b387987b59960c3e5fdcdfb9f4224e
Kalana Vimukthi Weerarathna <kalana.weerarathne@scicom.com.my>
2019-07-31 11:38:54 +0530

    -components

--- interest-service/src/main/resources/data_insert.sql
+++ interest-service/src/main/resources/data_insert.sql
@@ -1,3 +1,5 @@
+
+-- default password is admin
 INSERT INTO fiverr_dvneagu.users (id, created_at, updated_at, password, user_name, user_type) VALUES (1, '2019-07-31 11:34:32', null, '$2a$10$IAJ5kSGTV/RVuN30/Qit0.g5k5amRnuI497tXN5c/TP7R1aFreJZq', 'admin', 'ROLE_ADMIN');
 
 INSERT INTO fiverr_dvneagu.rates (id, created_at, updated_at, from_date, interest_rate, penalties) VALUES (1, '2019-07-30 15:46:40', null, '2017-03-05', 0.4, 0);
